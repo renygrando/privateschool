b66895eb484028a0e38ffa5ecca8b4e70e9cb8b3
Reny Grando <renygrando@gmail.com>
2020-06-25 23:32:54 -0300

    add funcionalidade de data e graduacao

---
+++ src/utils.js
@@ -0,0 +1,44 @@
+module.exports = {
+    
+    age: function age(timestamp){
+        const today = new Date()
+        const birthDate = new Date(timestamp)
+
+        let age = today.getFullYear() - birthDate.getFullYear()
+        const month = today.getMonth() - birthDate.getMonth()
+
+        if (month < 0 || month == 0 && today.getDate() < birthDate.getDate()) {
+            age = age -1
+        }
+        return age
+    },
+    graduation: function graduation(educationalLevel){
+        const graduation = [
+            " ",
+            "Ensino Médio Completo",
+            "Ensino Superior Completo",
+            "Mestrado",
+            "Doutorado",
+        ]
+
+        for(i = 0; i <= graduation.length ; i++ ) {
+            
+            if (educationalLevel == i ){
+                return graduation[i]
+            }
+            
+        }
+
+    },
+    date: function(timestamp) {
+        const date = new Date(timestamp)
+
+        const year = date.getUTCFullYear()
+
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+
+        const day = `0${date.getUTCDate()}`.slice(-2)
+
+        return `${year}-${month}-${day}`
+    }
+}
