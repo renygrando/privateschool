a7f1061a701d047f30530b232e23c4d7d0e44974
Reny Grando <renygrando@gmail.com>
2020-07-02 23:37:20 -0300

    add MethodOverrride e middleware

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
 const express= require('express')
 const nunjucks = require('nunjucks')
-const routes = require('./routes')
+const routes = require('./routes') 
+const methodOverride = require('method-override')
 const server = express()
 
 
 server.set('view engine', 'njk')
 
 server.use(express.urlencoded({extended: true}))
-server.use(routes)
 server.use(express.static('public'))
+server.use(methodOverride('_method'))
+server.use(routes)
 
 nunjucks.configure('src/views', {
     express: server,
